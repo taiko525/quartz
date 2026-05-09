37188bad9f82af8995cf08a2b8deef771bd413ce
taiko525 <taiko525@outlook.com>
2026-05-09 21:57:50 +0800

    chore‌: 更改字体

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,9 +23,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "JetBrains Mono" /* "Schibsted Grotesk" */ ,
+        body:   "JetBrains Mono" /* "Source Sans Pro"   */ ,
+        code:   "JetBrains Mono" /* "IBM Plex Mono"     */ ,
       },
       colors: {
         lightMode: {
